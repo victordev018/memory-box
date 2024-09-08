d694b622d224c68f7aae7d71952d3d4d85ccf1d0
João VIctor <joawrt8520@gmail.com>
2024-09-08 15:19:30 -0300

    feat: project created and config database

---
+++ src/main/java/com/jota/oscara/OscaraApplication.java
@@ -0,0 +1,13 @@
+package com.jota.oscara;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OscaraApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OscaraApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jota/oscara/OscaraApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jota.oscara;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OscaraApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
